0916f0207a819f54e7140fd6a805bef2556972aa
Movva Bharath <36327690+movva-bharath@users.noreply.github.com>
2022-01-03 12:48:41 +0530

    Javascript_Basics

---
+++ 01_Basics/variables.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+
+let name = "bharath"
+
+let age = "30"
+
+let score1 = 100
+
+let score2 = 100
+
+let totalScore = score1 + score2
+
+console.log(totalScore);
+
+let firstName = "Bharath"
+
+let lastName = "Movva"
+
+console.log(firstName + ' ' + lastName);
